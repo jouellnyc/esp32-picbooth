edc89f0e3a4cc9784741e368eb6fd9786e3419b5
JohnO <jouellnyc@gmail.com>
2022-11-20 09:56:10 -0500

    Increase Font

--- rpi/templates/upload.py
+++ rpi/templates/upload.py
@@ -1,15 +1,22 @@
+
 <!doctype html>
 <html lang=en>
 <head>
     <meta charset=utf-8>
     <title>Flask-Reuploaded Example</title>
+    <style>
+	input[type='text'] { font-size: 48px; }
+	input[type='file'] { font-size: 48px; }
+	input[type='submit'] { font-size: 48px; }
+    </style>
 </head>
 
 
 <body>
+
     {% with messages = get_flashed_messages() %}
     {% if messages %}
-    <ul class=flashes>
+    <ul class=flashes style="font-size:48px">
     {% for message in messages %}
         <li>{{ message }}</li>
     {% endfor %}
@@ -19,10 +26,9 @@
 
 <form method=POST enctype=multipart/form-data action="{{ url_for('upload') }}">
     <input type=file name=photo>
-    <button type="submit">Submit</button>
+    <button type="submit" style="font-size:48px"> Submit </button>
 </form>
 
-
 </body>
-</html>
 
+</html>
